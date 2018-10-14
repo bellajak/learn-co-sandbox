f4efe61a6405ab1779b08f73417fecd557ec46c6
reactive-proc-1585 <jakbellamy@gmail.com>
2018-10-14 20:25:42 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -12,11 +12,17 @@ def batch_badge_creator(names)
     puts "Hello, my name is #{name}"
   end
 end 
+
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-batch_badge_creator(names)
+# batch_badge_creator(names)
 
-def assign_rooms
-   names.each.with_index do |names|
-     puts "Hello, #{name}! You'll be assigned to room #{index}
+def assign_rooms(names)
+  names.each.with_index do |names|
+    puts "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   
+assign_rooms(names)
